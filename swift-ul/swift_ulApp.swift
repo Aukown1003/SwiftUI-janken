2668964ac6757010dff32cbb62795b82cb963026
Auk1003 <Aukown1003@users.noreply.github.com>
2023-03-13 15:49:03 +0900

    Initial Commit

---
+++ swift-ul/swift_ulApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ulApp.swift
+//  swift-ul
+//
+//  Created by 桑野友輔 on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct swift_ulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
